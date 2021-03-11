d1da7100d1fca5558209e67ae3d3535dde7a75c6
Vinicius George dos Santos <vinicius.george@sga.pucminas.br>
2021-03-11 15:44:10 +0000

    Create - Inserindo um registro com comando SQL, método: commit()

--- 03_create_data_sql.py
+++ 03_create_data_sql.py
@@ -7,22 +7,22 @@ cursor = conn.cursor()
 # inserindo dados na tabela
 cursor.execute("""
 INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf, criado_em)
-VALUES ('Afonso', 50, '00000000000', 'afonso@email.com', '31-98765-4321', 'Belo Horizonte', 'MG', '2021-03-11')
+VALUES ('Afonso', 50, '00000000000', 'afonso@email.com', '31-98765-4321', 'Belo Horizonte', 'MG', '2014-06-08')
 """)
 
 cursor.execute("""
 INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf, criado_em)
-VALUES ('Baltazar', 47, '11111111111', 'baltazar@email.com', '21-98765-4322', 'Rio de Janeiro', 'RJ', '2021-03-12')
+VALUES ('Julio', 47, '11111111111', 'julio@email.com', '11-98765-4322', 'São Paulo', 'SP', '2014-06-09')
 """)
 
 cursor.execute("""
 INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf, criado_em)
-VALUES ('Joaquim', 45, '22222222222', 'joaquim@email.com', '11-98765-4323', 'São Paulo', 'SP', '2021-03-13')
+VALUES ('Baltazar', 53, '22222222222', 'baltazar@email.com', '21-98765-4323', 'Rio de Janeiro', 'RJ', '2014-06-09')
 """)
 
 cursor.execute("""
 INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf, criado_em)
-VALUES ('Getulio', 54, '33333333333', 'getulio@email.com', '27-98765-4324', 'Vitória', 'ES', '2021-03-14')
+VALUES ('Getulio', 45, '33333333333', 'getulio@email.com', '27-98765-4324', 'Vitória', 'ES', '2014-06-08')
 """)
 
 # gravando no bd
